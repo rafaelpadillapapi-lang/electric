b2315da9894f60ae861987724b0b1b13c3b2c438
rafaelpadillapapi-lang <rafaelpadillapapi@gmail.com>
2026-01-18 04:24:31 -0500

    Connect Supabase anon key

--- auth.js
+++ auth.js
@@ -2,6 +2,8 @@ const SUPABASE_URL = "https://masknjbfxadqoashvrro.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hc2tuamJmeGFkcW9hc2h2cnJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MDY4MzQsImV4cCI6MjA4NDI4MjgzNH0.pII8GzC3xuhJViTlC5qPcwEgHb1TaNDdCs-PWyfbLxM";
 
+
+
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function signUp(email, password) {
